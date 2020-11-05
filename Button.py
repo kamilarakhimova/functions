7b36e48506016712a1322e6415ddc230ac298671
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2020-11-05 14:05:44 +0300

    Create Button.py

---
+++ Button.py
@@ -0,0 +1,9 @@
+class Button:
+    def __init__(self):
+        self.click = 0
+    def click(self):
+        self.click += 1
+    def click_count(self):
+        return self.click
+    def reset(self):
+        self.click = 0
