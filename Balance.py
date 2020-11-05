4c5f26b7e98f48a965f8eee25b406253ada72e0f
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2020-11-05 14:23:51 +0300

    Update Balance.py

--- Balance.py
+++ Balance.py
@@ -2,10 +2,13 @@ class Balance:
     def __init__(self):
         self.left = 0
         self.right = 0
+
     def add_right(self, right):
         self.right += right
+
     def add_left(self, left):
         self.left += left
+
     def result(self):
         if self.left == self.right:
             return '='
